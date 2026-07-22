d61978ce307a866b76391498d7cc7ed4fc227079
bahijabaj123 <benabdeljalilbahija@gmail.com>
2026-07-22 12:46:13 +0100

    Reduction empreinte memoire pour Render free tier

--- backend/src/main/java/com/carte/estimateurippbackend/EstimateurIppBackendApplication.java
+++ backend/src/main/java/com/carte/estimateurippbackend/EstimateurIppBackendApplication.java
@@ -2,10 +2,10 @@ package com.carte.estimateurippbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.thymeleaf.autoconfigure.ThymeleafAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@EnableScheduling
+@SpringBootApplication(exclude = { ThymeleafAutoConfiguration.class })@EnableScheduling
 public class EstimateurIppBackendApplication {
 
   public static void main(String[] args) {
